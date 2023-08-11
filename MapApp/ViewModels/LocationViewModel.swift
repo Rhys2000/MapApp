4d5b7b1026c155de07d4fbf291fb87ad8704a804
Rhys Julian-Jones <rhys2kjj@gmail.com>
2023-08-11 12:30:11 -0700

    Added Published variables mapLocation and mapRegion to LocationViewModel, created a private function to update mapRegion based on mapLocation

--- MapApp/ViewModels/LocationViewModel.swift
+++ MapApp/ViewModels/LocationViewModel.swift
@@ -6,11 +6,35 @@
 //
 
 import Foundation
+import MapKit
+import SwiftUI
 
 class LocationViewModel: ObservableObject {
+    
+    //All loaded locations
     @Published var locations: [Location]
+  
+    //Current location on map
+    @Published var mapLocation: Location {
+        didSet {
+            updateMapRegion(location: mapLocation)
+        }
+    }
+    
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
     
     init() {
-        self.locations = LocationsDataService.locations
+        let locationData = LocationDataService.locations
+        self.locations = locationData
+        self.mapLocation = locationData.first!
+        
+        self.updateMapRegion(location: locationData.first!)
+    }
+    
+    private func updateMapRegion(location: Location) {
+        withAnimation(.easeInOut) {
+            mapRegion = MKCoordinateRegion(center: location.coordinates, span: mapSpan)
+        }
     }
 }
