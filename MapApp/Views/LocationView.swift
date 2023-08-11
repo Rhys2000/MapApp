4e553825e0f799d0616dc5b44f74b163a488c2d0
Rhys Julian-Jones <rhys2kjj@gmail.com>
2023-08-11 13:26:36 -0700

    Added headerBar with the name of the current Location the mapRegion is zoomed in onto

--- MapApp/Views/LocationView.swift
+++ MapApp/Views/LocationView.swift
@@ -16,6 +16,11 @@ struct LocationView: View {
         ZStack {
             Map(coordinateRegion: $vm.mapRegion)
                 .ignoresSafeArea()
+            
+            VStack(spacing: 0) {
+                headerBar.padding()
+                Spacer()
+            }
         }
     }
 }
@@ -23,5 +28,35 @@ struct LocationView: View {
 struct LocationView_Previews: PreviewProvider {
     static var previews: some View {
         LocationView().environmentObject(LocationViewModel())
+    }
+}
+
+extension LocationView {
+    private var headerBar: some View {
+        VStack {
+            Button(action: vm.toggleLocationList) {
+                Text(vm.mapLocation.name + ", " + vm.mapLocation.cityName)
+                    .font(.title2)
+                    .fontWeight(.black)
+                    .foregroundColor(.primary)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                    .animation(.none, value: vm.mapLocation)
+                    .overlay(alignment: .leading) {
+                        Image(systemName: "arrow.down")
+                            .font(.headline)
+                            .foregroundColor(.primary)
+                            .padding()
+                            .rotationEffect(Angle(degrees: vm.showLocationList ? 180 : 0))
+                }
+            }
+              
+            if(vm.showLocationList) {
+                LocationListView()
+            }
+        }
+        .background(.thickMaterial)
+        .cornerRadius(10)
+        .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 15)
     }
 }
