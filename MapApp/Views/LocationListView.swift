2a8cb32a7ab2514289f90036adb56f2a62123926
Rhys Julian-Jones <rhys2kjj@gmail.com>
2023-08-11 13:25:47 -0700

    Created LocationListView which corresponds to each row of the list of locations that drops down from the headerBar

---
+++ MapApp/Views/LocationListView.swift
@@ -0,0 +1,55 @@
+//
+//  LocationListView.swift
+//  MapApp
+//
+//  Created by Rhys Julian-Jones on 8/11/23.
+//
+
+import SwiftUI
+
+struct LocationListView: View {
+    
+    @EnvironmentObject private var vm: LocationViewModel
+    
+    var body: some View {
+        List {
+            ForEach(vm.locations) { location in
+                Button {
+                    vm.showNextLocation(location: location)
+                } label: {
+                    locationListRow(location: location)
+                }
+                .padding(.vertical, 4)
+                .listRowBackground(Color.clear)
+            }
+        }
+        .listStyle(PlainListStyle())
+    }
+}
+
+struct LocationListView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationListView().environmentObject(LocationViewModel())
+    }
+}
+
+extension LocationListView {
+    private func locationListRow(location: Location) -> some View {
+        HStack {
+            if let imageName = location.imageNames.first {
+                Image(imageName)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 45, height: 45)
+                    .cornerRadius(10)
+            }
+            VStack(alignment: .leading) {
+                Text(location.name)
+                    .font(.headline)
+                Text(location.cityName)
+                    .font(.subheadline)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+        }
+    }
+}
