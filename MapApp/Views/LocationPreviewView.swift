570225e28100fa54f1ea4a39d7e023a5d4de1637
Rhys Julian-Jones <rhys2kjj@gmail.com>
2023-08-12 16:21:15 -0700

    Added functionality to the Learn More button

--- MapApp/Views/LocationPreviewView.swift
+++ MapApp/Views/LocationPreviewView.swift
@@ -74,7 +74,7 @@ extension LocationPreviewView {
     
     private var learnButton: some View {
         Button {
-            
+            vm.sheetLocation = location
         } label: {
             Text("Learn More")
                 .font(.headline)
